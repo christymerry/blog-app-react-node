14c17fdad7cb1f746310259b1d254c7e1b5aa9f7
christymerry <christymerry97@gmail.com>
2025-02-25 07:03:44 +0000

    authentication started

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.post('/register',(req,res)=>{
+    res.json('test ok2');
+});
+app.listen(4000);
--- client/blog_client/src/pages/Registerpage.js
+++ client/blog_client/src/pages/Registerpage.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Registerpage() {
+function Registerpage() {;
+  const[username,setUsername]=useState("");
+  const[password,setPassword]=useState("");
+  async function register(){
+    ev.preventDefault();
+    await fetch('http;//localhost:4000',{
+      method : 'POST',
+      body: JSON.stringify({username,password}),
+      headers:{'Content-Type':'application/json'},
+    })
+
+  }
   return (
-    <form  className="register">
-        <input type="text" placeholder='username'/>
-        <input type="text" placeholder='password'/>
-        <button>Register</button>
+    <form  className="register" on Sumbit={register}>
+        <input type="text" placeholder='username' value={username} onChange={ev =>setUsername(ev.target.value)}/>
+        <input type="text" placeholder='password' value={password} onChange={ev => setPassword(ev.target.value)}/>
+        <button >Register</button>
     </form>
   )
 }
