ad509593a778fca4580993713c7addd4d5d97e43
christymerry <christymerry97@gmail.com>
2025-03-04 07:05:50 +0000

    register authentication

--- api/index.js
+++ api/index.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
 
-app.post('/register',(req,res)=>{
-    res.json('test ok2');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://blog:febK4XM67qahM8qj@cluster0.yuw3j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+app.post('/register',async (req,res)=>{
+    const{username,password} =req.body;
+    try{ const userDoc = await User.create({
+        username,
+        password:bcrypt.hashSync(password,salt)
+    });
+    res.json(userDoc);
+    }catch(e){
+        console.log(e)
+        res.status(400).json(e);
+
+    }
+    
 });
+
+app.post('/login',async (req,res)=>{
+    const{username,password} =req.body;
+    const userDoc = await User.findOne({username});
+    res.json(userDoc)
+})
 app.listen(4000);
+//febK4XM67qahM8qj
+
+
+//mongodb+srv://blog:febK4XM67qahM8qj@cluster0.yuw3j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- client/blog_client/src/pages/Registerpage.js
+++ client/blog_client/src/pages/Registerpage.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react'
+import bcrypt from "bcryptjs";
 
 function Registerpage() {;
   const[username,setUsername]=useState("");
   const[password,setPassword]=useState("");
-  async function register(){
+
+  async function register(ev){
     ev.preventDefault();
-    await fetch('http;//localhost:4000',{
+    const response = await fetch('http://localhost:4000/register',{
       method : 'POST',
       body: JSON.stringify({username,password}),
       headers:{'Content-Type':'application/json'},
     })
+    console.log(response)
+    if(response.status!== 200){
+      alert("Registeration failed")
+    }else{
+      alert('Registeration successfull')
+    }
 
   }
   return (
-    <form  className="register" on Sumbit={register}>
+    <form  className="register" onSubmit={register}>
         <input type="text" placeholder='username' value={username} onChange={ev =>setUsername(ev.target.value)}/>
         <input type="text" placeholder='password' value={password} onChange={ev => setPassword(ev.target.value)}/>
         <button >Register</button>
